26cf5b8af470e6f5e08469eb313083392e41c1da
Vivian Munguía <andy_munguia@hotmail.com>
2021-02-23 00:06:21 -0700

    Creación de la clase VarLet.swift
    Se probaron algunos tipos de variables

---
+++ VarLet.swift
@@ -0,0 +1,16 @@
+//Declaración de una constante
+let maximumNumberOfLogicAttempts = 3
+//Declaración de una variable
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+//La variable será de tipo String, pero aún no se sabe su valor
+var welcomeMessage : String
+//Se le asigna un valor a la variable
+welcomeMessage = "Hola, ¿qué tal?"
+
+var red, green, blue : Double
+
+π = 3.14
+
+print(π)
